efcb590f2d5b120ea73f9885be0f1419a7087777
NorthernLatitude40 <49724984+NorthernLatitude40@users.noreply.github.com>
2026-08-19 19:07:31 +0900

    fix bug payment_status (#86)
    Co-authored-by: ww <bluedream20074754@gmail.com>

--- src/api/v1/endpoints/inventory_api.py
+++ src/api/v1/endpoints/inventory_api.py
@@ -50,7 +50,7 @@ from src.model.inventory_schema import StockListResponse, SellDeviceResponse, Ad
 from src.api.auth_api import get_current_staff   # 直連 staff 的驗證依賴
 from src.common.generate_sn import generate_sn
 from src.model.inventory_schema import UpdateStatusRequest,SellDeviceConfirmPayload
-from src.common.dict import StockStatusEnum
+from src.common.dict import StockStatusEnum, PaymentStatusEnum
 
 logger = logging.getLogger(__name__)
 
@@ -484,6 +484,7 @@ async def confirm_sell_device(
             profit=profit,
             business_type=1,
             business_id=outbound_order.id,
+            payment_status=PaymentStatusEnum.PAYED.value,
             payment_method=payload.payment_method,
             remark=f"设备出售出库：{device.title} | 订单号:{order_sn}",
             shop_id=x_shop_id,
